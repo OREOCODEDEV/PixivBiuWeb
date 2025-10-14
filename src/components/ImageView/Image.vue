7a97595e8abafed0270ad8b8357f0f97c44d437e
OREOCODEDEV <17620745509@126.COM>
2025-10-15 06:22:55 +0800

    格式化ImageVue

--- src/components/ImageView/Image.vue
+++ src/components/ImageView/Image.vue
@@ -5,7 +5,7 @@ const props = defineProps(["image_data", "modal_ref"]);
 </script>
 
 <template>
-    <div class="flex flex-col">
+    <div class="flex min-w-20 flex-col">
         <div class="relative flex overflow-hidden rounded-xl">
             <div class="flex grow self-stretch">
                 <img class="aspect-square grow rounded-xl object-cover object-top" :src="props.image_data.image_urls.medium.replace('https://i.pximg.net', 'https://i.pixiv.re')" />
@@ -20,17 +20,17 @@ const props = defineProps(["image_data", "modal_ref"]);
             <div class="absolute bottom-0 left-0 right-0 top-0 z-10 cursor-pointer hover:bg-white/25" @click="props.modal_ref.show(props.image_data)">
                 <div class="flex h-full w-full flex-row items-end font-bold text-white">
                     <div class="flex grow">
-                        <span class="px-2 py-1">{{props.image_data.total_bookmarked}}</span>
-                        <span class="px-2 py-1">{{props.image_data.total_viewed}}</span>
+                        <span class="px-2 py-1">{{ props.image_data.total_bookmarked }}</span>
+                        <span class="px-2 py-1">{{ props.image_data.total_viewed }}</span>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="flex flex-col">
-            <span class="cursor-pointer truncate text-nowrap pt-2 font-bold" @click="props.modal_ref.show(props.image_data)">
+        <div class="flex min-w-20 flex-col">
+            <span class="cursor-pointer truncate pt-2 font-bold" @click="props.modal_ref.show(props.image_data)">
                 {{ props.image_data.title }}
             </span>
-            <span class="cursor-pointer truncate text-nowrap pt-1 text-xs text-gray-500" @click="props.modal_ref.show(props.image_data)">
+            <span class="cursor-pointer truncate pt-1 text-xs text-gray-500" @click="props.modal_ref.show(props.image_data)">
                 {{ props.image_data.author.name }}
             </span>
         </div>
