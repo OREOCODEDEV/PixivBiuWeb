6d1c9b6de24954593b90349c37e9812a06beba58
OREOCODEDEV <17620745509@126.COM>
2025-10-18 09:31:29 +0800

    General commit

--- src/views/SettingsView/Browse.vue
+++ src/views/SettingsView/Browse.vue
@@ -22,22 +22,22 @@ import SelectFromMulti from "@/components/General/SelectFromMulti.vue";
 <template>
     <div class="max-w-130 w-130 flex flex-col py-4">
         <span class="title">预览大小</span>
-        <span class="description">设置在搜索预览时展示的图片尺寸大小，设置为小可同时预览更多图片</span>
+        <span class="description">在搜索预览时展示的图片尺寸大小，设置为小可同时预览更多图片</span>
         <SelectFromMulti :options="{ 小: 'small', 中: 'medium', 大: 'large' }" current_value="medium" />
     </div>
     <div class="max-w-130 w-130 flex flex-col py-4">
         <span class="title">预览分辨率</span>
-        <span class="description">设置在搜索预览时展示的图片分辨率，如果无对应版本则按照不大于选项的最高分辨率版本显示</span>
+        <span class="description">在搜索预览时展示的图片分辨率，如果无对应版本则按照不大于选项的最高分辨率版本显示</span>
         <SelectFromMulti :options="{ 小: 'small', 中: 'medium', 大: 'large', 原图: 'origin' }" current_value="medium" />
     </div>
     <div class="max-w-130 w-130 flex flex-col py-4">
-        <span class="title">大图分辨率</span>
-        <span class="description">设置在查看大图时是否尝试加载更高分辨率版本，如设置不加载则与预览分辨率保持一致以最大程度减少缓冲</span>
+        <span class="title">查看分辨率</span>
+        <span class="description">在查看大图时是否尝试加载更高分辨率版本，如设置不加载则与预览分辨率保持一致以最大程度减少缓冲</span>
         <SelectFromMulti :options="{ 不加载: 'keep', 加载: 'load' }" current_value="load" />
     </div>
     <div class="max-w-130 w-130 flex flex-col py-4">
         <span class="title">固定分类栏</span>
-        <span class="description">设置滚动查看图片时，分类栏是否固定在顶部而不跟随图片内容滑出屏幕</span>
-        <SelectFromMulti :options="{ 不加载: 'keep', 加载: 'load' }" current_value="load" />
+        <span class="description">滚动查看图片时，分类栏是否固定在顶部而不跟随图片内容滑出屏幕</span>
+        <SelectFromMulti :options="{ 不固定: 'scroll', 固定: 'sticky' }" current_value="sticky" />
     </div>
 </template>
--- src/views/SettingsView/Filter.vue
+++ src/views/SettingsView/Filter.vue
@@ -27,14 +27,14 @@ import SelectFromMulti from "@/components/General/SelectFromMulti.vue";
     </div>
     <div class="max-w-130 w-130 flex flex-col py-4">
         <span class="title">搜索页数</span>
-        <span class="description">设置搜索时从源站抓取的页面数量，设置越大则获取的图片数量越多，数据排名越客观，但容易引起风控及系统卡顿。建议设置合适数值。</span>
+        <span class="description">搜索时从源站抓取的页面数量，设置越大则获取的图片数量越多，数据排名越客观，但容易引起风控及系统卡顿。建议设置合适数值。</span>
         <div class="flex items-stretch rounded-xl bg-blue-50 p-6">
             <input type="text" :value="storage.getItem('settings_proxy_server') ? storage.getItem('settings_proxy_server') : '5'" class="h-full appearance-none focus:outline-none" />
         </div>
     </div>
     <div class="max-w-130 w-130 flex flex-col py-4">
         <span class="title">R18筛选模式</span>
-        <span class="description">设置筛选R18标签时是否重新请求图片集以获取更多图片</span>
+        <span class="description">筛选R18标签时是否重新请求图片集以获取更多图片</span>
         <SelectFromMulti :options="{ 不请求: 'frontend', 请求: 'backend' }" current_value="frontend" />
     </div>
 </template>
